ceef829bd2e81c38097af4f5a8b366603747d378
gpugs-dev <lexaost@bk.ru>
2025-05-22 12:47:09 +0300

    Create chapter_1.js

---
+++ chapter_1.js
@@ -0,0 +1,50 @@
+# ГЛАВА 1
+
+# 2.1
+print("Добро пожаловать в Study Script!")
+
+# 2.2
+print("  ***  ")
+print(" *   * ")
+print(" *   * ")
+print(" ***** ")
+print(" *   * ")
+print(" *   * ")
+print(" *   * ")
+
+# 2.3 
+b = "B"
+i = "I"
+n = "N"
+g = "G"
+o = "O"
+print(b + "-" + i + "-" + n + "-" + g + "-" + o)
+
+# 2.4
+cardHolder = "IVAN PETROV"
+cardNumber = "1234 5678 9012 3456"
+expDate = "12/25"
+cvv = "123"
+print("┌──────────────────────────────┐")
+print("│   БАНКОВСКАЯ КАРТА          │")
+print("│                              │")
+print("│  " + cardNumber + "       │")
+print("│                              │")
+print("│  Срок действия: " + expDate + "         │")
+print("│  Держатель: " + cardHolder + "      │")
+print("│                              │")
+print("│  CVV: " + cvv + "                     │")
+print("└──────────────────────────────┘")
+
+# 2.5 
+print("3 x 1 = " + (3*1))
+print("3 x 2 = " + (3*2))
+print("3 x 3 = " + (3*3))
+print("3 x 4 = " + (3*4))
+print("3 x 5 = " + (3*5))
+print("3 x 6 = " + (3*6))
+print("3 x 7 = " + (3*7))
+print("3 x 8 = " + (3*8))
+print("3 x 9 = " + (3*9))
+print("3 x 10 = " + (3*10))
+
